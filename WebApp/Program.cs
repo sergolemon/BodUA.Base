4bc9ce365b0d80398950507cbb3287e7db566889
Sergo Lemon <srg.lemn@gmail.com>
2024-01-22 21:32:16 +0200

    add blazor

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages(cfg => {
+    cfg.RootDirectory = "/";
+});
+
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseStaticFiles();
+
+app.UseEndpoints(endpoints => {
+    endpoints.MapFallbackToPage("/Host");
+    endpoints.MapBlazorHub();
+});
 
 app.Run();
